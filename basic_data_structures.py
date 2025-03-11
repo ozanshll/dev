cb39bbb235bc931abadd5bcdbfb168ac6edb4b21
ozanshll <116595268+ozanshll@users.noreply.github.com>
2025-03-11 11:35:15 +0100

    dev

---
+++ basic_data_structures.py
@@ -0,0 +1,24 @@
+# Integer
+my_age = 22  # int
+
+# Floating point
+my_height = 7.4  # float
+
+# String
+my_name = "adil"  # str
+
+# List
+my_favorite_numbers = [5, 14, 21]  # list
+
+# Dictionary
+my_profile = {"name": "adil", "age": 22}  # dict
+
+# Tuple
+my_coordinates = (20.718, -74.0000)  # tuple
+
+# Set
+my_unique_numbers = {1, 2, 3, 4}  # set
+
+# Boolean
+is_student = True  # bool
+
